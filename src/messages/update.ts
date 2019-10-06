cf1056e505106cb750c905b6a8c8ad3bf1bca2a7
Helgard Ferreira <hrfer3@gmail.com>
2019-10-06 20:05:54 +0200

    Modify extension id references

--- src/messages/update.ts
+++ src/messages/update.ts
@@ -7,42 +7,47 @@ import * as i18n from './../i18n';
 
 /** Show the update message if the icon theme has been updated. */
 export const showUpdateMessage = () => {
-    // if the user does not want to see the update message
-    if (helpers.getThemeConfig('showUpdateMessage').globalValue !== true) return;
+  // if the user does not want to see the update message
+  if (helpers.getThemeConfig('showUpdateMessage').globalValue !== true) return;
 
-    vscode.window.showInformationMessage(
-        i18n.translate('themeUpdated'),
+  vscode.window
+    .showInformationMessage(
+      i18n.translate('themeUpdated'),
 
-        // show 'Activate' button if icon theme is not active
-        (versioning.checkVersionSupport('1.10.0') && helpers.isThemeNotVisible())
-            ? i18n.translate('activate') : undefined,
+      // show 'Activate' button if icon theme is not active
+      versioning.checkVersionSupport('1.10.0') && helpers.isThemeNotVisible()
+        ? i18n.translate('activate')
+        : undefined,
 
-        i18n.translate('readChangelog'),
-        i18n.translate('neverShowAgain')
-    ).then(handleUpdateMessageActions);
+      i18n.translate('readChangelog'),
+      i18n.translate('neverShowAgain'),
+    )
+    .then(handleUpdateMessageActions);
 };
 
 /** Handle the actions of the update message. */
-const handleUpdateMessageActions = (value) => {
-    switch (value) {
-        case i18n.translate('activate'):
-            activateIcons();
-            break;
+const handleUpdateMessageActions = value => {
+  switch (value) {
+    case i18n.translate('activate'):
+      activateIcons();
+      break;
 
-        case i18n.translate('readChangelog'):
-            open('https://marketplace.visualstudio.com/items/PKief.material-icon-theme/changelog');
-            break;
+    case i18n.translate('readChangelog'):
+      open(
+        'https://marketplace.visualstudio.com/items/helgardrichard.helium-icon-theme/changelog',
+      );
+      break;
 
-        case i18n.translate('neverShowAgain'):
-            disableUpdateMessage();
-            break;
+    case i18n.translate('neverShowAgain'):
+      disableUpdateMessage();
+      break;
 
-        default:
-            break;
-    }
+    default:
+      break;
+  }
 };
 
 /** Disable the update messages in the global settings */
 const disableUpdateMessage = () => {
-    helpers.setThemeConfig('showUpdateMessage', false, true);
+  helpers.setThemeConfig('showUpdateMessage', false, true);
 };
